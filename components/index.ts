63225ed097295ff4d1352129f09b1ba14b3b2ebf
Ranivoaritida Sandy <ranivoaritidasandy@gmail.com>
2023-07-03 16:37:12 +0300

    searchBar

--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,15 @@ import Hero from './Hero';
 import CustomButton from './CustomButton';
 import Footer from './Footer';
 import Navbar from './Navbar';
-
+import SearchBar from './SearchBar';
+import CustomFilter from './CustomFilter';
+import SearchManufacturer from './SearchManufacturer';
 export {
     Hero,
     CustomButton,
     Footer,
     Navbar,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
 }
