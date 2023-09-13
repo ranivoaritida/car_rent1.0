9d343d54ed81019c5055bde227973161ed706513
Ranivoaritida Sandy <ranivoaritidasandy@gmail.com>
2023-09-13 14:29:49 +0300

    searchbar

--- utils/index.ts
+++ utils/index.ts
@@ -6,7 +6,7 @@ export async function fetchCars(){
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',{
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q7',{
         headers:headers,
     });
 
@@ -32,5 +32,16 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   };
 
 export const generateCarImageUrl = (car:CarProps,angle?: string) =>{
+    const url = new URL('https://cdn.imagin.studio/getimage');
+
+    const {make, year, model } = car;
+
+    url.searchParams.append('customer','hrjavascript-mastery');
+    url.searchParams.append('make',make);
+    url.searchParams.append('modelFamily',model.split(' ')[0]);
+    url.searchParams.append('zoomType','fullscreen');
+    url.searchParams.append('modelYear',`${year}`);
+    url.searchParams.append('angle',`${angle}`);
     
+    return `${url}`;
 }
